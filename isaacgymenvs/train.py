d5d8d3a37978898c88e185532eebe19ee9ff2b1d
Costa Huang <costah@nvidia.com>
2022-04-18 08:06:59 -0700

    Fix `train.py`

--- isaacgymenvs/train.py
+++ isaacgymenvs/train.py
@@ -55,12 +55,6 @@ from isaacgymenvs.learning import amp_network_builder
 ## OmegaConf & Hydra Config
 
 # Resolvers used in hydra configs (see https://omegaconf.readthedocs.io/en/2.1_branch/usage.html#resolvers)
-OmegaConf.register_new_resolver('eq', lambda x, y: x.lower()==y.lower())
-OmegaConf.register_new_resolver('contains', lambda x, y: x.lower() in y.lower())
-OmegaConf.register_new_resolver('if', lambda pred, a, b: a if pred else b)
-# allows us to resolve default arguments which are copied in multiple places in the config. used primarily for
-# num_ensv
-OmegaConf.register_new_resolver('resolve_default', lambda default, arg: default if arg=='' else arg)
 
 @hydra.main(config_name="config", config_path="./cfg")
 def launch_rlg_hydra(cfg: DictConfig):
