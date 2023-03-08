810965695abdce60401a00e1e521b858a2fb880a
Daniel Tan <dtch1997@users.noreply.github.com>
2023-03-08 12:38:54 +0000

    Modify download script to enable downloading multiple checkpoints

--- isaacgymenvs/download.py
+++ isaacgymenvs/download.py
@@ -1,6 +1,6 @@
 """ Simple script to download WandB checkpoint"""
 
-import os
+import pathlib
 import argparse 
 import wandb
 
@@ -14,4 +14,12 @@ if __name__ == "__main__":
     api = wandb.Api()
     run = api.run(f"{args.wandb_run_path}")
     model = run.file(args.wandb_filename)
-    model.download(".")
+    model.download(".", replace=True)
+    
+    # Move the download to new directory
+    download_fp = pathlib.Path(args.wandb_filename)
+    output_dir = pathlib.Path('downloads')
+    output_dir = output_dir / str(run.id)
+    output_dir.mkdir(exist_ok=True, parents=True) 
+    output_fp = output_dir / download_fp.name
+    download_fp.rename(output_fp)
