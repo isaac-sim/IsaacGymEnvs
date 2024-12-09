1c3ec1c422cdc28efff8ab1b4424db20bfcf2cee
Xi Chen <407081305@qq.com>
2024-12-09 16:27:46 +0800

    code on readme page to test programming env ready

---
+++ debug/dbg_create_env.py
@@ -0,0 +1,20 @@
+import isaacgym
+import isaacgymenvs
+import torch
+
+num_envs = 2000
+
+envs = isaacgymenvs.make(
+	seed=0, 
+	task="Ant", 
+	num_envs=num_envs, 
+	sim_device="cuda:0",
+	rl_device="cuda:0",
+)
+print("Observation space is", envs.observation_space)
+print("Action space is", envs.action_space)
+obs = envs.reset()
+for _ in range(20):
+	random_actions = 2.0 * torch.rand((num_envs,) + envs.action_space.shape, device = 'cuda:0') - 1.0
+	envs.step(random_actions)
+ 
